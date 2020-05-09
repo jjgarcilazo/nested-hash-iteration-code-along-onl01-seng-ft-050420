385a96d3f6ba17203a29d586e6370a89d5e5398c
Jesse Garcilazo <jjgarcilazo@gmail.com>
2020-05-09 18:47:02 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -14,7 +14,7 @@ require 'pry'
      }
    }
 
-   ddef remove_strawberry(contacts)
+   def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     if person == "Freddy Mercury"
       contact_details_hash.each do |attribute, data|
