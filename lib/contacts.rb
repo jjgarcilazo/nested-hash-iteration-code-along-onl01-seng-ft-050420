b6e31b59733b6a140b3edd331c5b750196278b9d
Jesse Garcilazo <jjgarcilazo@gmail.com>
2020-05-09 18:43:41 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -10,14 +10,14 @@ require 'pry'
      "Freddy Mercury" => {
        name: "Freddy",
        email: "freddy@mercury.com",
-       favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+       favorite_ice_cream_flavors: [ "cookie dough", "mint chip"]
      }
    }
 
    def remove_strawberry(contacts)
      # Assign Freddy Mercury's hash to a variable
      freddy = contacts["Freddy Mercury"]
-
+   
      # Iterate over only Freddy Mercury's data
      freddy.each do |attribute, value|
        if attribute == :favorite_icecream_flavors
