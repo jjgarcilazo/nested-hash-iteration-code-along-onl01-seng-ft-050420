330d4a2a1eeded1fcab438dfc06643991d7072f8
Jesse Garcilazo <jjgarcilazo@gmail.com>
2020-05-09 18:32:40 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -14,12 +14,15 @@ require 'pry'
   #   }
   # }
 
-def remove_strawberry(contacts)
-  contacts.each do |person, contact_details_hash|
-    contact_details_hash.each do |attribute, data|
+  def remove_strawberry(contacts)
+    # Assign Freddy Mercury's hash to a variable
+    freddy = contacts["Freddy Mercury"]
+  
+    # Iterate over only Freddy Mercury's data
+    freddy.each do |attribute, value|
       if attribute == :favorite_icecream_flavors
-        data.delete_if { |ice_cream| ice_cream == "strawberry"}
+        value.delete_if{ |ice_cream| ice_cream == "strawberry" }
       end
     end
-  end
-end
+    contacts
+  end 
